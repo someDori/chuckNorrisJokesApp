246b0b210bf5e40ec9f6b6f00eca9770a7ef903c
Demetre-Panjakidze <demetrepanjakidze1@gmail.com>
2024-02-05 16:30:39 +0400

    removed all code in tests

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,12 +1,1 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:my_app/app/app.dart';
-import 'package:my_app/jokeGenerator/counter.dart';
 
-// void main() {
-//   group('App', () {
-//     testWidgets('renders JokeGeneratorPage', (tester) async {
-//       await tester.pumpWidget(const App());
-//       expect(find.byType(JokeGeneratorPage), findsOneWidget);
-//     });
-//   });
-// }
