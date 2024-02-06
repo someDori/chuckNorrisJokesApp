e2c604026ea64fafe072d4ad071d8b7eb4b2e55a
Demetre-Panjakidze <demetrepanjakidze1@gmail.com>
2024-02-06 16:29:19 +0400

    fixed test

--- test/counter/cubit/counter_cubit_test.dart
+++ test/counter/cubit/counter_cubit_test.dart
@@ -1,1 +1,19 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
 
+import 'package:my_app/jokeGenerator/counter.dart';
+
+void main() {
+  group('CounterCubit', () {
+    test('initial state is empty string', () {
+      expect(JokeGenetatorCubit().state, equals(''));
+    });
+
+    blocTest<JokeGenetatorCubit, String>(
+      'emits passed text when emit is called',
+      build: JokeGenetatorCubit.new,
+      act: (cubit) => cubit.jokeGenerator('test'),
+      expect: () => [equals('test')],
+    );
+  });
+}
